9b77468a77493fbf681fdbb38a5ebea4bde7f607
Irbea192 <elepop777@gmail.com>
2025-07-14 10:32:49 +0900

    AreaエンティティにManualとのリレーションを追加し、nameカラムにインデックスを設定

--- backend/src/entity/Area.ts
+++ backend/src/entity/Area.ts
@@ -1,14 +1,20 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, Index } from 'typeorm';
 import { ZipCode } from './ZipCode';
+import { Manual } from './Manual';
 
 @Entity('AREAS')
 export class Area {
     @PrimaryGeneratedColumn({ type: 'int' })
     id!: number;
 
-    @Column({ type: 'varchar', length: 100, unique: true })
+    @Index('IDX_AREAS_name', { unique: true }) // DB上のインデックス名を指定
+    @Column({ type: 'varchar', length: 100 })
     name!: string; // '大阪市' など
 
     @OneToMany(() => ZipCode, (zipCode) => zipCode.area)
     zipCodes!: ZipCode[];
+
+    // Manualとの1対多リレーションを追加
+    @OneToMany(() => Manual, (manual) => manual.area)
+    manuals!: Manual[];
 }
