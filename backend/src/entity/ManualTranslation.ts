1ec5335abcad8bd3138147476630c9f76d86eec6
Irbea192 <elepop777@gmail.com>
2025-07-14 10:33:23 +0900

    ManualTranslationエンティティのカラム定義を修正し、リレーションにnullableとonDeleteオプションを追加

--- backend/src/entity/ManualTranslation.ts
+++ backend/src/entity/ManualTranslation.ts
@@ -7,23 +7,29 @@ export class ManualTranslation {
     @PrimaryGeneratedColumn({ type: 'int' })
     id!: number;
 
-    @Column({ type: 'varchar', length: 255 })
+    @Column({ type: 'varchar', length: 256 })
     garbage!: string; // 表示用のゴミ名前
 
-    @Column({ type: 'varchar', length: 255 })
+    @Column({ type: 'varchar', length: 50 })
     type!: string; // 表示用の分別区分
 
     @Column({ type: 'text', nullable: true })
     contents!: string | null; // 表示用の分別注意文
 
-    @Column({ type: 'varchar', length: 4, nullable: true })
+    @Column({ type: 'varchar', length: 4 })
     word!: string; // 表示用のキーワード
 
-    @ManyToOne(() => Manual, (manual) => manual.translations)
-    @JoinColumn({ name: 'manual_id' })
+    @ManyToOne(() => Manual, (manual) => manual.translations, {
+        nullable: false, // DBがNOT NULLなのでfalseを指定
+        onDelete: 'CASCADE', // DBの制約に合わせる
+    })
+    @JoinColumn({ name: 'manual_id', referencedColumnName: 'id', foreignKeyConstraintName: 'FK_manual_translation_to_manual' })
     manual!: Manual;
 
-    @ManyToOne(() => Language)
-    @JoinColumn({ name: 'language_id' })
+    @ManyToOne(() => Language, {
+        nullable: false, // DBがNOT NULLなのでfalseを指定
+        onDelete: 'RESTRICT', // DBの制約に合わせる
+    })
+    @JoinColumn({ name: 'language_id', referencedColumnName: 'id', foreignKeyConstraintName: 'FK_manual_translation_to_language' })
     language!: Language;
 }
