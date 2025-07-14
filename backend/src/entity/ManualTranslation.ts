d62d6f537f45c8a8907100e6ae2c7535467cf53d
Irbea192 <elepop777@gmail.com>
2025-07-14 11:02:21 +0900

    ManualTranslationエンティティのgarbageおよびtypeカラムの長さを修正し、wordカラムをnullableに変更

--- backend/src/entity/ManualTranslation.ts
+++ backend/src/entity/ManualTranslation.ts
@@ -7,16 +7,16 @@ export class ManualTranslation {
     @PrimaryGeneratedColumn({ type: 'int' })
     id!: number;
 
-    @Column({ type: 'varchar', length: 256 })
+    @Column({ type: 'varchar', length: 255 })
     garbage!: string; // 表示用のゴミ名前
 
-    @Column({ type: 'varchar', length: 50 })
+    @Column({ type: 'varchar', length: 255 })
     type!: string; // 表示用の分別区分
 
     @Column({ type: 'text', nullable: true })
     contents!: string | null; // 表示用の分別注意文
 
-    @Column({ type: 'varchar', length: 4 })
+    @Column({ type: 'varchar', length: 4, nullable: true })
     word!: string; // 表示用のキーワード
 
     @ManyToOne(() => Manual, (manual) => manual.translations, {
